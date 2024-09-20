ab390c388ea345ebb67faf7f229e9bdddd82e14b
Sidpatchy <35241490+Sidpatchy@users.noreply.github.com>
2024-09-20 16:00:51 -0600

    Update retard.py

--- retard.py
+++ retard.py
@@ -6,3 +6,9 @@ def make_retards(n):
 
   return retards
 # djcows 19-sept-2024
+
+# sidpatchy 2024-09-20
+def djcows_doesnt_know_how_to_python(n): return ['retard'] * n
+
+def djcows_doesnt_know_how_to_python_but_worse(n): return ['retard' for _ in range(n)]
+# sidpatchy 2024-09-20
