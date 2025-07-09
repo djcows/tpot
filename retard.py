5ec5db365b141f1e27157cb29939237859d0a634
notthepeanutbar <notthepeanutbar@proton.me>
2025-07-10 01:41:59 +0700

    add generate_retard_with_extra_steps function

--- retard.py
+++ retard.py
@@ -12,3 +12,11 @@ def djcows_doesnt_know_how_to_python(n): return ['retard'] * n
 
 def djcows_doesnt_know_how_to_python_but_worse(n): return ['retard' for _ in range(n)]
 # sidpatchy 2024-09-20
+
+# notthepeanutbar 2024-07-10
+def generate_retard_with_extra_steps():
+    letters = ['r', 'e', 't', 'a', 'r', 'd']
+    word = ""
+    for letter in letters:
+        word = word + letter
+    return word
